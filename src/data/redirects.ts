b10170fe84e7fe1a2329ba1083b44fb76cecea53
Katto <kattogamet@gmail.com>
2025-02-17 13:41:35 -0300

    change redirects config and add uuhcm2 form redirect

--- src/data/redirects.ts
+++ src/data/redirects.ts
@@ -2,5 +2,13 @@ export const redirects = [
     {
         id: "discord",
         url: "https://discord.gg/sKRYYyVCnw",
+        title: "Discord",
+        desc: "Discord de la comunidad de Ban Game Studios.",
+    },
+    {
+        id: "uuhcm2",
+        url: "https://forms.gle/Av1Y89SvBtDDEJjq6",
+        title: "Un UHC Mas 2",
+        desc: "Formulario para participar en Un UHC Más 2.",
     }
 ]
