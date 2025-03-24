09fb3c078f49a97dd0ffe8070ca711caf381da6c
Katto <kattogamet@gmail.com>
2025-03-24 12:57:37 -0300

    add ttt form

--- src/data/redirects.ts
+++ src/data/redirects.ts
@@ -4,5 +4,11 @@ export const redirects = [
         url: "https://discord.gg/sKRYYyVCnw",
         title: "Discord",
         desc: "Discord de la comunidad de Ban Game Studios.",
+    },
+    {
+        id: "ttt",
+        url: "https://forms.gle/qSPDTHjmodydGz8S6",
+        title: "TTT",
+        desc: "Formulario para participar en el evento TTT en Minecraft."
     }
 ]
