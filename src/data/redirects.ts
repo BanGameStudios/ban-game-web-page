6a88e72315bbdf13502f399f972fcb15711eddba
Katto <kattogamet@gmail.com>
2025-05-17 12:20:38 -0300

    add flower power

--- src/data/redirects.ts
+++ src/data/redirects.ts
@@ -4,5 +4,11 @@ export const redirects = [
         url: "https://discord.gg/sKRYYyVCnw",
         title: "Discord",
         desc: "Discord de la comunidad de Ban Game Studios.",
+    },
+    {
+        id: "fp",
+        url: "https://forms.gle/FfvRKojYhYgTTygC6",
+        title: "Flower Power",
+        desc: "¡Prepara tus habilidades y conocimientos para está experiencia visual completamente repleta de flores!"
     }
 ]
